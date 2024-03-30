dbb240eed5f70183cc18b4e25bb2f5d0b9c4ef98
bestia.dev <info@bestia.dev>
2024-03-30 09:16:25 -0500

    sshadd

--- configuration_files/debian_files/.ssh/sshadd.sh
+++ configuration_files/debian_files/.ssh/sshadd.sh
@@ -12,18 +12,18 @@ printf "  \033[33m It is recommanded to use the ~/.ssh/config file to assign exp
 printf "  \033[33m If not, ssh-agent will send all the keys to the server and the server could refute the connection because of too many bad keys. \033[0m\n"
 
 # check the content of ~/.ssh/config if it contains the ssh keys
-cat ~/.ssh/config | grep -q "~/.ssh/crustde_rustdevuser_ssh_1" || printf "  \033[31m The ~/.ssh/config does not contain the identity ~/.ssh/crustde_rustdevuser_ssh_1. \033[0m\n"
 cat ~/.ssh/config | grep -q "~/.ssh/github_com_git_ssh_1" || printf "  \033[31m The ~/.ssh/config does not contain the identity ~/.ssh/github_com_git_ssh_1. \033[0m\n"
 cat ~/.ssh/config | grep -q "~/.ssh/bestia_dev_luciano_bestia_ssh_1" || printf "  \033[31m The ~/.ssh/config does not contain the identity ~/.ssh/bestia_dev_luciano_bestia_ssh_1. \033[0m\n"
-
-# add if key not yet exist in ssh-agent for rustdevuser@localhost:2201
-ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/crustde_rustdevuser_ssh_1 | awk '{print $2}'` || ssh-add -t 1h ~/.ssh/crustde_rustdevuser_ssh_1
+cat ~/.ssh/config | grep -q "~/.ssh/crustde_rustdevuser_ssh_1" || printf "  \033[31m The ~/.ssh/config does not contain the identity ~/.ssh/crustde_rustdevuser_ssh_1. \033[0m\n"
 
 # add if key not yet exist in ssh-agent for git@github.com
 ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/github_com_git_ssh_1 | awk '{print $2}'` || ssh-add -t 1h ~/.ssh/github_com_git_ssh_1
 
 # add if key not yet exist in ssh-agent for luciano_bestia@bestia.dev
 ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/bestia_dev_luciano_bestia_ssh_1 | awk '{print $2}'` || ssh-add -t 1h ~/.ssh/bestia_dev_luciano_bestia_ssh_1
+
+# add if key not yet exist in ssh-agent for rustdevuser@localhost:2201
+ssh-add -l | grep -q `ssh-keygen -lf ~/.ssh/crustde_rustdevuser_ssh_1 | awk '{print $2}'` || ssh-add -t 1h ~/.ssh/crustde_rustdevuser_ssh_1
 
 printf "  \033[33m The keys are set to expire in 1 hour. \033[0m\n"
 printf "  \033[33m For security, when you are finished using the keys, remove them from the ssh-agent with: \033[0m\n"
